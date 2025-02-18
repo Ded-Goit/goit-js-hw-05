1e81cc0702c4e999d8baa3a8b0803c53e6e5dc50
Andriy Romanov <mgm.agro04@gmail.com>
2025-02-19 00:56:14 +0200

    task-1

--- js/task-1.js
+++ js/task-1.js
@@ -1,10 +1,18 @@
 `use strict`;
-/* Виконуй це завдання у файлі task-1.js
-Напиши стрілочну функцію getUserNames(users), яка прийматиме один параметр users — масив об’єктів
-користувачів. Функція має повертати масив імен усіх користувачів (властивість name) із масиву users.
-Візьми код нижче і встав після оголошення своєї функції для перевірки коректності її роботи.
-У консоль будуть виведені результати її викликів.*/
-
+/* Write an arrow function getUserNames(users) that will accept one parameter users — an array of users objects.
+The function should return an array of names of all users (the name property) from the users array.
+Take the code below and insert it after the declaration of your function to verify that it works correctly.
+The results of its calls will be printed to the console.*/
+/** We declare an arrow function with a single parameter users.
+ *  We return the result of the map call.
+ *  The map method creates a new array in which each element
+ *  is the value of the name property of each user.
+ * The function iterates over the array of objects,
+ * extracts the value of the name key from each object,
+ * and returns a new array of these values.*/
+const getUserNames = users => {
+  return users.map(user => user.name);
+};
 console.log(
   getUserNames([
     {
